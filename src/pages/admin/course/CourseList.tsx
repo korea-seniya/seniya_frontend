1c524b16e460fcb35e129735f44d275de56f9ead
buzz9248 <buzz9248@naver.com>
2025-06-04 14:49:24 +0900

    feat: pages 추가

---
+++ src/pages/admin/course/CourseList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CourseList() {
+  return (
+    <div>
+      CourseList(관리자)
+    </div>
+  )
+}
+
+export default CourseList
---
+++ src/pages/admin/course/CreateCourse.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateCourse() {
+  return (
+    <div>
+      CreateCourse(관리자)
+    </div>
+  )
+}
+
+export default CreateCourse
