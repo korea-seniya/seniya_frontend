cc76357a69962ecbae8d2b63d1af243ff107c0e7
chang0506 <j8970@naver.com>
2025-06-09 17:31:30 +0900

    feat:pages-inquiry-create

---
+++ src/pages/inquiry/InquiryDetail.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function InquiryDetail() {
+  return (
+    <div>InquiryDetail</div>
+  )
+}
+
+export default InquiryDetail
---
+++ src/pages/inquiry/inquiryUpdate.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function inquiryUpdate() {
+  return (
+    <div>inquiryUpdate</div>
+  )
+}
+
+export default inquiryUpdate
