8e2a2b22866c2c6a97a28b009be8bdd438ec1ba0
iasos2 <ykkminji@gmail.com>
2025-06-23 17:09:45 +0900

    feature/update-participationpage

--- src/App.tsx
+++ src/App.tsx
@@ -30,14 +30,16 @@ import EmailVerification from './pages/user/EmailVerification';
 import TrainerProfile from "./pages/trainer/profile/TrainerProfile"
 import GetTrainerProfile from "./pages/trainer/profile/GetTrainerProfile"
 import PutTrainerProfile from "./pages/trainer/profile/PutTrainerProfile"
+import MyParticipationList from "./pages/participation/MyParticipationList"
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/api/v1/posts/:id' element={<PostDetail />} />
-        <Route path='signup' element={<SignUp />} />
-        <Route path='signIn' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
         <Route path="/email-send" element={<EmailSend />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/verify-email" element={<EmailVerification />} />
@@ -56,6 +58,7 @@ function App() {
         <Route path='/api/v1/trainer-profile' element={<TrainerProfile />} />
         <Route path='/api/v1/trainer-profile/me' element={<GetTrainerProfile />} />
         <Route path='/api/v1/trainer-profile/update' element={<PutTrainerProfile />} />
+        <Route path='/myparticipation'element={<MyParticipationList />} />
         
       
         
